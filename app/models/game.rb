ec4a81561f984376de09776e7a4d531c754a3800
yuichiro19 <78407847+yuichiro19@users.noreply.github.com>
2021-03-18 17:33:59 +0900

    gameモデルのバリデーションとアソシエーションの定義

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,16 @@
 class Game < ApplicationRecord
+    extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to :user
+    belongs_to :rank_bonus
+    belongs_to :rate
+    belongs_to :top_bonus
+
+    with_options presence: do
+        validates :guest1
+        validates :guest2
+        validates :guest3
+        validates :rank_bonus_id
+        validates :top_bonus_id
+        validates :rate_id
+    end
 end
