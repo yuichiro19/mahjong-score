f5e09dfe991bcf37c3a5d9337fec42340d4cf768
yuichiro19 <78407847+yuichiro19@users.noreply.github.com>
2021-03-18 17:05:56 +0900

    rateモデルidの修正

--- app/models/rate.rb
+++ app/models/rate.rb
@@ -2,12 +2,12 @@ class Rate < ActiveHash::Base
     include ActiveHash::Associations
     has_many :games
     self.data = [
-        { id: 0, name: "ノーレート" },
-        { id: 1, name: 1 },
-        { id: 2, name: 3 },
-        { id: 3, name: 5 },
-        { id: 4, name: 10 },
-        { id: 5, name: 20 },
-        { id: 6, name: 30 }
+        { id: 1, name: "ノーレート" },
+        { id: 2, name: 1 },
+        { id: 3, name: 3 },
+        { id: 4, name: 5 },
+        { id: 5, name: 10 },
+        { id: 6, name: 20 },
+        { id: 7, name: 30 }
     ]
 end
