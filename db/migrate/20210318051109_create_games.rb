4403b73587e95c18c61ce4defcd09e97d05d13fc
yuichiro19 <78407847+yuichiro19@users.noreply.github.com>
2021-03-18 14:41:47 +0900

    Gamesモデル、マイグレーション

--- db/migrate/20210318051109_create_games.rb
+++ db/migrate/20210318051109_create_games.rb
@@ -1,7 +1,14 @@
 class CreateGames < ActiveRecord::Migration[6.0]
   def change
     create_table :games do |t|
-
+      t.string :guest1, null: false
+      t.string :guest2, null: false
+      t.string :guest3, null: false
+      t.integer :rank_bonus_id, null: false
+      t.integer :top_bonus_id, null: false
+      t.integer :rate_id, null: false
+      t.integer :chip, null: false
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_17_095922) do
+ActiveRecord::Schema.define(version: 2021_03_18_051109) do
+
+  create_table "games", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "guest1", null: false
+    t.string "guest2", null: false
+    t.string "guest3", null: false
+    t.integer "rank_bonus_id", null: false
+    t.integer "top_bonus_id", null: false
+    t.integer "rate_id", null: false
+    t.integer "chip", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_games_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,4 +39,5 @@ ActiveRecord::Schema.define(version: 2021_03_17_095922) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "games", "users"
 end
