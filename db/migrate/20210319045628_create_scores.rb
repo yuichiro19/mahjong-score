dd77393c7f48d80b1ca1a7266987566c4956ba10
yuichiro19 <78407847+yuichiro19@users.noreply.github.com>
2021-03-19 14:37:13 +0900

    scoreテーブルのマイグレート

--- db/migrate/20210319045628_create_scores.rb
+++ db/migrate/20210319045628_create_scores.rb
@@ -1,7 +1,20 @@
 class CreateScores < ActiveRecord::Migration[6.0]
   def change
     create_table :scores do |t|
-
+      t.integer :user_score, null: false
+      t.integer :guest1_score, null: false
+      t.integer :guest2_score, null: false
+      t.integer :guest3_score, null: false
+      t.integer :user_rank_id, null: false
+      t.integer :guest1_rank_id, null: false
+      t.integer :guest2_rank_id, null: false
+      t.integer :guest3_rank_id, null: false
+      t.integer :user_point, null: false
+      t.integer :guest1_point, null: false
+      t.integer :guest2_point, null: false
+      t.integer :guest3_point, null: false
+      t.references :user, null: false, foreign_key: true
+      t.references :game, null: false, foreign_key: true
       t.timestamps
     end
   end
