79681815c0d9d66058b84129bcbb588b8f98a3a5
yuichiro19 <78407847+yuichiro19@users.noreply.github.com>
2021-03-22 18:00:13 +0900

    createアクションの定義、ストロングパラメーターの設定

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -5,6 +5,13 @@ class ScoresController < ApplicationController
     end
 
     def create
+        Score.create(score_params)
+        redirect_to new
+    end
+
+    private 
+    def score_params
+        params.require(:score).permit(:user_score, :guest1_score, :guest2_score, :guest3_score, :user_rank_id, :guest1_rank_id, :guest2_rank_id, :guest3_rank_id, :user_point, :guest1_point, :guest2_point, :guest3_point).merge(user_id: current_user.id, game_id: params[:@game_id])
     end
 
 end
