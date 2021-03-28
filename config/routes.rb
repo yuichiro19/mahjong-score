1d681d0a7d629cceac5ee9b674eec57edab5adc3
yuichiro19 <78407847+yuichiro19@users.noreply.github.com>
2021-03-28 17:32:41 +0900

    showアクションのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'games/index'
   root to: "games#index"
+  resources :users, only: :show
   resources :games, only: [:index, :new, :create] do
     resources :scores, only: [:index, :create, :show]
   end
