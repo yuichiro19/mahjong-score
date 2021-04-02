1e3b3619287077ba54854adfdb78bae7fe7f8566
yuichiro19 <78407847+yuichiro19@users.noreply.github.com>
2021-04-02 17:11:14 +0900

    個人成績表示のためのshowアクション内の定義

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
     def show
+        @user = User.find(params[:id])
+        @scores = @user.scores
     end
+end
+
+private
+def ave_scores(a)
+    @scores.sum{|hash| hash[a]}.fdiv(@scores.length).round(2)
 end
