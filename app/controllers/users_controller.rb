b8df0743c3d42f42b77f8da8a30c7845e6a8d6b6
yuichiro19 <78407847+yuichiro19@users.noreply.github.com>
2021-04-03 13:31:08 +0900

    マイページ可読性の向上

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,9 @@ class UsersController < ApplicationController
     def show
         @user = User.find(params[:id])
         @scores = @user.scores
-    end
+        @user_ranks = []
+        @scores.each do |rank|
+            @user_ranks << rank[:user_rank]
+        end
+    end 
 end
