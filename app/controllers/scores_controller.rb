3c5b70f5b82dffb91a685ce43a60b695e0a2484c
yuichiro19 <78407847+yuichiro19@users.noreply.github.com>
2021-04-30 17:10:59 +0900

    ポイント計算のメソッド可読性の向上

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -44,73 +44,40 @@ class ScoresController < ApplicationController
 
   def point_calc
     if params[:score][:user_rank] == '1'
-      guest1_point_calc
-      guest2_point_calc
-      guest3_point_calc
+      params[:score][:guest1_point] = player_point_calc(params[:score][:guest1_score], params[:score][:guest1_rank])
+      params[:score][:guest2_point] = player_point_calc(params[:score][:guest2_score], params[:score][:guest2_rank])
+      params[:score][:guest3_point] = player_point_calc(params[:score][:guest3_score], params[:score][:guest3_rank])
       params[:score][:user_point] =
         -(params[:score][:guest1_point] + params[:score][:guest2_point] + params[:score][:guest3_point])
     elsif params[:score][:guest1_rank] == '1'
-      user_point_calc
-      guest2_point_calc
-      guest3_point_calc
+      params[:score][:user_point] = player_point_calc(params[:score][:user_score], params[:score][:user_rank])
+      params[:score][:guest2_point] = player_point_calc(params[:score][:guest2_score], params[:score][:guest2_rank])
+      params[:score][:guest3_point] = player_point_calc(params[:score][:guest3_score], params[:score][:guest3_rank])
       params[:score][:guest1_point] =
         -(params[:score][:user_point] + params[:score][:guest2_point] + params[:score][:guest3_point])
     elsif params[:score][:guest2_rank] == '1'
-      user_point_calc
-      guest1_point_calc
-      guest3_point_calc
+      params[:score][:user_point] = player_point_calc(params[:score][:user_score], params[:score][:user_rank])
+      params[:score][:guest1_point] = player_point_calc(params[:score][:guest1_score], params[:score][:guest1_rank])
+      params[:score][:guest3_point] = player_point_calc(params[:score][:guest3_score], params[:score][:guest3_rank])
       params[:score][:guest2_point] =
         -(params[:score][:user_point] + params[:score][:guest1_point] + params[:score][:guest3_point])
     else
-      user_point_calc
-      guest1_point_calc
-      guest2_point_calc
+      params[:score][:user_point] = player_point_calc(params[:score][:user_score], params[:score][:user_rank])
+      params[:score][:guest1_point] = player_point_calc(params[:score][:guest1_score], params[:score][:guest1_rank])
+      params[:score][:guest2_point] = player_point_calc(params[:score][:guest2_score], params[:score][:guest2_rank])
       params[:score][:guest3_point] =
         -(params[:score][:user_point] + params[:score][:guest1_point] + params[:score][:guest2_point])
     end
   end
 
-  def user_point_calc
-    user_base_point = ((params[:score][:user_score].to_i - 100).round(-3) - @game.top_bonus.basic) / 1000
-    params[:score][:user_point] = if params[:score][:user_rank] == '2'
-                                    user_base_point + @game.rank_bonus.second_bonus
-                                  elsif params[:score][:user_rank] == '3'
-                                    user_base_point + @game.rank_bonus.third_bonus
-                                  else
-                                    user_base_point + @game.rank_bonus.forth_bonus
-                                  end
-  end
-
-  def guest1_point_calc
-    guest1_base_point = ((params[:score][:guest1_score].to_i - 100).round(-3) - @game.top_bonus.basic) / 1000
-    params[:score][:guest1_point] = if params[:score][:guest1_rank] == '2'
-                                      guest1_base_point + @game.rank_bonus.second_bonus
-                                    elsif params[:score][:guest1_rank] == '3'
-                                      guest1_base_point + @game.rank_bonus.third_bonus
-                                    else
-                                      guest1_base_point + @game.rank_bonus.forth_bonus
-                                    end
-  end
-
-  def guest2_point_calc
-    guest2_base_point = ((params[:score][:guest2_score].to_i - 100).round(-3) - @game.top_bonus.basic) / 1000
-    params[:score][:guest2_point] = if params[:score][:guest2_rank] == '2'
-                                      guest2_base_point + @game.rank_bonus.second_bonus
-                                    elsif params[:score][:guest2_rank] == '3'
-                                      guest2_base_point + @game.rank_bonus.third_bonus
-                                    else
-                                      guest2_base_point + @game.rank_bonus.forth_bonus
-                                    end
-  end
-
-  def guest3_point_calc
-    guest3_base_point = ((params[:score][:guest3_score].to_i - 100).round(-3) - @game.top_bonus.basic) / 1000
-    params[:score][:guest3_point] = if params[:score][:guest3_rank] == '2'
-                                      guest3_base_point + @game.rank_bonus.second_bonus
-                                    elsif params[:score][:guest3_rank] == '3'
-                                      guest3_base_point + @game.rank_bonus.third_bonus
-                                    else
-                                      guest3_base_point + @game.rank_bonus.forth_bonus
-                                    end
+  def player_point_calc(score, rank)
+    base_point = ((score.to_i - 100).round(-3) - @game.top_bonus.basic) / 1000
+    if rank == '2'
+      base_point + @game.rank_bonus.second_bonus
+    elsif rank == '3'
+      base_point + @game.rank_bonus.third_bonus
+    else
+      base_point + @game.rank_bonus.forth_bonus
+    end
   end
 end
